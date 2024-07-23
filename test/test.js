237134ea1eb477270f4aaaff8c386d528d63ef9b
junhee kim <loplove7@naver.com>
2024-07-23 14:21:15 +0900

    0723

---
+++ test/test.js
@@ -0,0 +1,13 @@
+let v1 = 0x6211002bb464e5842ff32c8b9b98d2a2892e33e4;
+let v2 = '0x6211002bb464e5842ff32c8b9b98d2a2892e33e4';
+
+console.log(v1);
+console.log(v1.toString(16));
+console.log(v2);
+console.log(v1+' ');
+
+let a = ['a','b'];
+for(var i in a){
+    console.log(i);
+    console.log(a[i]);
+}
